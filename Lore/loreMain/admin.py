1f401c6e3c6dba22d2a400bc5a22b16bf1752713
priyaven <priyaven@gmail.com>
2019-02-09 12:11:50 -0800

    Added database admin to create stories for testing.

--- Lore/loreMain/admin.py
+++ Lore/loreMain/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import InterestedUsers, Story, StoryChapters
+
+admin.site.register(InterestedUsers)
+admin.site.register(Story)
+admin.site.register(StoryChapters)
