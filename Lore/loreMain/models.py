937d829431910f2c29c9ba070c269b9e21116732
priyaven <priyaven@gmail.com>
2019-03-21 20:07:36 -0700

    Fixed story chapters model
    Now it has nested objects which is great. prev, yes, no chapters are all story chapter objects. no more MPTT.

--- Lore/loreMain/models.py
+++ Lore/loreMain/models.py
@@ -14,14 +14,20 @@ class Story(models.Model):
     author = models.CharField(max_length=100)
 
 
-class StoryChapters(MPTTModel):
+class StoryChapters(models.Model):
     story_id = models.ForeignKey(Story, on_delete=models.CASCADE)
     chapter_number = models.IntegerField()
     chapter_title = models.CharField(max_length=100)
+    prev_chapter = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+    yes_chapter = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+    no_chapter = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+    depth = models.IntegerField()
+    '''
     prev_chapter = TreeForeignKey('self', null=True, blank=True, 
         related_name='children', db_index=True, on_delete=models.CASCADE)
     yes_chapter = models.PositiveIntegerField(null=True, blank=True)
     no_chapter = models.PositiveIntegerField(null=True, blank=True)
+    '''
     #yes_chapter = models.ForeignKey('self', null=True, blank=True, 
      #   related_name='+', on_delete=models.CASCADE)
     #no_chapter = models.ForeignKey('self', null=True, blank=True, 
@@ -30,9 +36,9 @@ class StoryChapters(MPTTModel):
     #yes_chapter = models.IntegerField(blank=True, null=True) #models.ForeignKey('StoryChapters', on_delete=models.CASCADE)
     #no_chapter = models.IntegerField(blank=True, null=True) #models.ForeignKey('StoryChapters', on_delete=models.CASCADE)
 
-    class MPTTMeta:
-        parent_attr = 'prev_chapter'
-        order_insertion_by = ['chapter_number']
+    #class MPTTMeta:
+        #parent_attr = 'prev_chapter'
+        #order_insertion_by = ['chapter_number']
         #left_attr = 'no_chapter'
         #right_attr = 'yes_chapter'
 
